62d5f4510d5fb619f4e28c784378a08b49f47c81
s-d-august <sinclairaugust@gmail.com>
2024-05-11 16:33:03 -0500

    added text response at '/'

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const app = express();
 
 app.get('/movies', (req, res, next) => {
     res.json()
-})
+});
 
+app.get('/', (req, res, next) => {
+    res.send('This is a default text response.')
+});
