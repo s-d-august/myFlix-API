44a026b16eb5d4b1c370ec9eee526fba6a457b2e
s-d-august <sinclairaugust@gmail.com>
2024-05-11 16:47:58 -0500

    required morgan

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-const express = require('express');
+const express = require('express'),
+    morgan = require('morgan');
 const app = express();
+
+app.use(express.static('public'));
+
+app.use(morgan('common'));
 
 app.get('/movies', (req, res, next) => {
     res.json()
@@ -9,9 +14,7 @@ app.get('/', (req, res, next) => {
     res.send('This is a default text response.')
 });
 
-app.use(express.static('public'));
-
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!')
-})
+});
