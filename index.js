a27503230ed3187d5e542fd1e55620fbe634f6ff
s-d-august <sinclairaugust@gmail.com>
2024-05-12 09:47:53 -0500

    added app.listen

--- index.js
+++ index.js
@@ -18,3 +18,7 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!')
 });
+
+app.listen(8080, () => {
+    console.log('Listening on port 8080.')
+})
