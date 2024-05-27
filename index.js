9e007d917da36b3ff8d790f1a755b1e779f5c707
s-d-august <sinclairaugust@gmail.com>
2024-05-27 14:33:46 -0500

    updated listener

--- index.js
+++ index.js
@@ -220,6 +220,8 @@ app.use((err, req, res, next) => {
 });
 
 // Listener
-app.listen(8080, () => {
-    console.log('Listening on port 8080.')
-})
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+ console.log('Listening on Port ' + port);
+});
+
