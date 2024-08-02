b60319749997168edad673df943f3161b80f4a15
s-d-august <sinclairaugust@gmail.com>
2024-08-02 14:50:54 -0500

    tried a quick fix...?

--- auth.js
+++ auth.js
@@ -1,21 +1,21 @@
 const jwtSecret = 'your_jwt_secret';
 
-const jwt = require('jsonwebtoken'),
-  passport = require('passport');
+import { sign } from 'jsonwebtoken';
+import { authenticate } from 'passport';
 
-require('./passport.js');
+import './passport.js';
 
 let generateJWTToken = (user) => {
-  return jwt.sign(user, jwtSecret, {
+  return sign(user, jwtSecret, {
     subject: user.username,
     expiresIn: '7d',
     algorithm: 'HS256'
   })
 }
 
-module.exports = (router) => {
+export default (router) => {
   router.post('/login', (req, res) => {
-    passport.authenticate('local', { session: false }, (error, user, info) => {
+    authenticate('local', { session: false }, (error, user, info) => {
       if (error || !user) {
         return res.status(400).json({
           message: 'Something is not right',
