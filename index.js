811865b4fa18269344a4c125071ab0148eabe056
s-d-august <sinclairaugust@gmail.com>
2024-08-31 07:27:07 -0500

    set allowedOrigins in CORS

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ require('dotenv').config()
 // mongoose.connect('mongodb://localhost:27017/myflix');
 mongoose.connect(process.env.CONNECTION_URI)
 const cors = require('cors');
+let allowedOrigins = ['http://localhost:1234'];
 const app = express();
 const Movies = Models.Movie;
 const Users = Models.User;
@@ -18,7 +19,18 @@ app.use(morgan('common'));
 app.use(bodyParser.json());
 
 
-app.use(cors());
+
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = "The CORS policy for this application doesn't allow access from origin " + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
